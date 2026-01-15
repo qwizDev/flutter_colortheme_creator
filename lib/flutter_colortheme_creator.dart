8d754ddf4fc526797e86622bf009d21cbc1a8726
christian <qwizdev@pm.de>
2026-01-15 16:41:48 +0100

    learning how to use packages in flutter: playing around

--- lib/flutter_colortheme_creator.dart
+++ lib/flutter_colortheme_creator.dart
@@ -1,5 +1,13 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+
+class DoSomeStuff {
+
+  // final someText = "helloMyFrend";
+
+  String makeAString() => "aaaaaaaaaaaaaaaaaa";
+
 }
