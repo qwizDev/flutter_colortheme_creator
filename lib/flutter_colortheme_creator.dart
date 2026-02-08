dbb2cca780ddaefc32a16d1293b69eda6c21df75
christian <qwizdev@pm.de>
2026-02-08 19:45:32 +0100

    improved PanelDevStd a lot, now all colorschemes can get listed and previewed

--- lib/flutter_colortheme_creator.dart
+++ lib/flutter_colortheme_creator.dart
@@ -1,3 +1,6 @@
-export 'src/view/show_all_theme_colors_widget.dart' show ShowAllThemeColorsWidget;
+export 'src/view/show_all_theme_colors_widget.dart'
+    show ShowAllThemeColorsWidget;
 export 'src/view/theme_color_creator_widget.dart' show ThemeColorCreatorWidget;
 export 'src/provider/theme_controller.dart' show ThemeController;
+export 'src/model/colorscheme_key.dart' show ColorSchemeKey;
+export 'src/model/colorscheme_helper.dart' show ColorSchemeHelper;
